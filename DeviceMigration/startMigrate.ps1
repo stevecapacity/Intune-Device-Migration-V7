43c70ee61e717947951c3d519ce7f8bb9e757467
stevecapacity <steve_weiner@outlook.com>
2024-08-28 10:51:56 -0400

    Update startMigrate.ps1

--- DeviceMigration/startMigrate.ps1
+++ DeviceMigration/startMigrate.ps1
@@ -430,10 +430,15 @@ else
     log "Target tenant headers not found.  Getting new user object from $tenant tenant..."
     $newHeaders = $sourceHeaders
 }
-$userLookup = $($currentUser.upn)
+$fullUPN = $($currentUser.upn)
+$split = $fullUPN -split "(@)", 2
+$split[0] += $split[1].Substring(0,1)
+$split[1] += $split[1].Substring(1)
+$userLookup = $split[0]
+log "Looking up user where UPN starts with: $userLookup..."
 $newUserObject = Invoke-RestMethod -Method GET -Uri "https://graph.microsoft.com/beta/users?`$filter=startsWith(userPrincipalName,'$userLookup')" -Headers $newHeaders
 # if new user graph request is successful, set new user object
-if($newUserObject.value -ne $null)
+if($null -ne $newUserObject.value)
 {
     log "New user found in $tenant tenant."
     $newUser = @{
@@ -490,44 +495,63 @@ else
     {
         log "Az.Accounts module already installed."
     }
-    schtasks.exe /create /tn "userFinder" /xml "C:\ProgramData\IntuneMigration\userFinder.xml" /f | Out-Host
     $newUserPath = "C:\Users\Public\Documents\newUserInfo.json"
-    do 
-    {        
-        Write-Host "New user info not found. Sleeping for 7 seconds..."
-        Start-Sleep -Seconds 7
-    } while (!(Test-Path $newUserPath))
-    
-    $newUserInfo = Get-Content -Path "C:\Users\Public\Documents\newUserInfo.json" | ConvertFrom-JSON
-
-    $newUser = @{
-        entraUserID = $newUserInfo.entraUserId
-        SID = $newUserInfo.SID
-        SAMName = $newUserInfo.SAMName
-        UPN = $newUserInfo.upn
-    }
-
-    foreach($x in $newUser.Keys)
+    $timeout = 300
+    $checkInterval = 5
+    $elapsedTime = 0
+    schtasks.exe /create /tn "userFinder" /xml "C:\ProgramData\IntuneMigration\userFinder.xml" /f | Out-Host
+    while($elapsedTime -lt $timeout)
     {
-        $newUserName = "NEW_$($x)"
-        $newUserValue = $($newUser[$x])
-        if(![string]::IsNullOrEmpty($newUserValue))
+        if(Test-Path $newUserPath)
         {
-            log "Writing $($newUserName) with value $($newUserValue)..."
-            try
-            {
-                reg.exe add "HKLM\SOFTWARE\IntuneMigration" /v $newUserName /t REG_SZ /d $newUserValue /f | Out-Null
-                log "Successfully wrote $($newUserName) with value $($newUserValue)."
-            }
-            catch
-            {
-                $message = $_.Exception.Message
-                log "Failed to write $($newUserName) with value $($newUserValue).  Error: $($message)."
-            }
+            log "New user found.  Continuing with script..."
+            break
+        }
+        else
+        {
+            log "New user info not present.  Waiting for user to sign in..."
+            Start-Sleep -Seconds $checkInterval
+            $elapsedTime += $checkInterval
         }
     }
-    Write-Host "User found. Continuing with script..."
-    Disable-ScheduledTask -TaskName "userFinder"
+    if(Test-Path $newUserPath)
+    {
+        $newUserInfo = Get-Content -Path "C:\Users\Public\Documents\newUserInfo.json" | ConvertFrom-JSON
+
+        $newUser = @{
+            entraUserID = $newUserInfo.entraUserId
+            SID = $newUserInfo.SID
+            SAMName = $newUserInfo.SAMName
+            UPN = $newUserInfo.upn
+        }
+        foreach($x in $newUser.Keys)
+        {
+            $newUserName = "NEW_$($x)"
+            $newUserValue = $($newUser[$x])
+            if(![string]::IsNullOrEmpty($newUserValue))
+            {
+                log "Writing $($newUserName) with value $($newUserValue)..."
+                try
+                {
+                    reg.exe add "HKLM\SOFTWARE\IntuneMigration" /v $newUserName /t REG_SZ /d $newUserValue /f | Out-Null
+                    log "Successfully wrote $($newUserName) with value $($newUserValue)."
+                }
+                catch
+                {
+                    $message = $_.Exception.Message
+                    log "Failed to write $($newUserName) with value $($newUserValue).  Error: $($message)."
+                }
+            }
+        }
+        Write-Host "User found. Continuing with script..."
+        Disable-ScheduledTask -TaskName "userFinder"
+        Remove-Item -Path $newUserPath -Force -Recurse
+    }
+    else
+    {
+        log "New user not found.  Exiting script."
+        exitScript -exitCode 4 -functionName "newUser"
+    }
 }       
 
 # Remove MDM certificate if present
